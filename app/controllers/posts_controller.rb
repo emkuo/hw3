d28f261ac8d5faedb0e55e5ced62eeecd382a965
emkuo <emily.kuo@kellogg.northwestern.edu>
2022-05-17 21:36:54 +0000

    final - success!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,18 @@ class PostsController < ApplicationController
 
 
 def new
-    @post =Post.new
-    @place = Place.find_by({"id" => params["place_id"]})
-    @post["place_id"] = @place["id"]
+    @post = Post.new
+    @post.place_id = params["place_id"]
 end
 
 def create
-puts params
-    @post = Post.new
+    @post = Post.new(params["post"])
     @post["title"] = params["post"]["title"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["description"] = params["post"]["description"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
-redirect_to "/places/#{@post["place_id"]}"
+redirect_to "/places/#{@post.place_id}"
 end
 
 end
