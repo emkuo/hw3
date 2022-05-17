4bb5f494f1a728fe64249041cd3614ac837f48fa
emkuo <emily.kuo@kellogg.northwestern.edu>
2022-05-17 20:44:51 +0000

    commit 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+
+def index
+    @places = Place.all
+end
+
+def show
+    @place = Place.find_by({ "id" => params["id"] })
+    #need to figure out why the below code line is failing 
+    @posts = Post.where({"place_id" => @place["id"]})
+end
+
+def new
+    @place = Place.new
+end
+
+def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+    redirect_to "/places"
+  end
+end 
+
